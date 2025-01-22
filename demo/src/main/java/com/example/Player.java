030ef6cf0ce96c67d600033a2720cfd57eadb175
Tatrite <florian.fuchs31700@gmail.com>
2025-01-22 10:56:10 +0100

    fisrt commit

---
+++ demo/src/main/java/com/example/Player.java
@@ -0,0 +1,18 @@
+package com.example;
+
+public class Player {
+    private int score;
+    private String name;
+    private boolean lost;
+    
+    public Player(String name){
+        score=0;
+        this.name=name;
+        this.lost=false;
+    }
+
+    public void addPoint(int x){
+        this.score+=x;
+        if 
+    }
+}
